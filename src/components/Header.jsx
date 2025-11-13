9c81187d3a450b9a248541a3523efcd1033ad6cf
Gautham Joshy <gauthamjoshy32@gmail.com>
2025-11-13 23:27:18 +0530

    started homePage design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiTeacher } from 'react-icons/gi'
 import { Link } from 'react-router-dom'
 
-function Header() {
+function Header({logoutVisibility}) {
   return (
     <>
-    <div className='w-full h-18 bg-blue-200 flex justify-start items-center'>
+    <div className='w-full h-18 bg-blue-200 flex justify-between items-center'>
         <Link to={"/"} id='header' className='flex'><GiTeacher  className='text-3xl md:ms-18 ms-5'/><h1 className='font-bold text-xl ms-2'>StudentManager</h1></Link>
+
+        {logoutVisibility&&
+            <div className='me-10'>
+            <button className='shadow px-3 py-2 bg-red-500 text-white font-medium rounded-lg hover:border-red-500 hover:bg-white hover:text-red-500 cursor-pointer transition'>Logout</button>
+        </div>
+        }
     </div>
     </>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 function HomePage() {
+      const [logoutVisibility, setLogoutVisibility] = useState(true)
   return (
     <>
-    <Header/>
+  <Header logoutVisibility = {logoutVisibility}/>
 
     <div className='bg-gray-200'>
+      <div className='flex justify-between p-5 md:ps-10 md:pe-10'>
+        <div>
+          <h1 className='font-bold md:text-3xl'>StudentManagement Dashboard</h1>
+          <h2 className='md:text-lg'>Manage Your Students Effectively</h2>
+        </div>
+        <h1 className='md:text-4xl font-extrabold'>Welcome <span>Alice</span>...!</h1>
+
+      </div>
 
     </div>
 
