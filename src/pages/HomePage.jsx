f7dd199422bc83ac9309f5b175b01ceb5db91300
Gautham Joshy <gauthamjoshy32@gmail.com>
2025-11-14 22:20:14 +0530

    search icon removed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -30,13 +30,13 @@ function HomePage() {
                 <p className='font-semibold text-gray-700'>Search Students</p>
 
               </div>
-              <div className='flex items-center'>
+              {/* <div className='flex items-center'> */}
                 <div className='flex justify-between items-center rounded border-2 border-gray-500 p-1 focus-within: focus-within:shadow focus-within:border-blue-600 mt-1'>
                   <input type="text" placeholder='Search by student name...' className='border-none outline-none w-full focus:border' />
 
                 </div>
-                <div className='flex justify-center items-center border h-9 rounded w-9 mt-1 bg-blue-300 cursor-pointer'><FaSearch /></div>
-              </div>
+                {/* <div className='flex justify-center items-center border h-9 rounded w-9 mt-1 bg-blue-300 cursor-pointer'><FaSearch /></div> */}
+              {/* </div> */}
 
             </div>
             <div className=' p-4 shadow rounded bg-gray-20 hover:scale-102 hover:shadow-blue-500 hover:shadow-lg transition'>
@@ -116,7 +116,7 @@ function HomePage() {
         <div className='md:px-10 p-5 gap-10'>
           <div className='grid md:grid-cols-2 grid-cols-1 gap-10'>
             {/* student card */}
-            <div className='md:mt-0 mt-2 bg-blue-200 rounded-lg shadow hover:shadow-lg hover:shadow-blue-700 hover:scale-102 transition'>
+            <div className='md:mt-0 mt-2 bg-linear-to-br from-red-200 to-blue-400 rounded-lg shadow hover:shadow-lg hover:shadow-blue-700 hover:scale-102 transition'>
               {/* outer */}
               <div className=''>
                 <div className='flex justify-between items-center gap-2 md:mt-15'>
@@ -124,15 +124,15 @@ function HomePage() {
                     <img className='w-full h-full object-cover' width={"100px"} height={"100px"} style={{ borderRadius: "50%" }} src="https://www.corporatephotographerslondon.com/wp-content/uploads/2022/02/FRA-1699dark-sq.jpg" alt="" />
                   </div>
                   <div className='grid md:gap-1 gap-1 md:me-20'>
-                    <div className='bg-gray-200 p-2'>
+                    <div className='bg-white p-2'>
                       <p className='font-semibold'>Student Name : <span className='font-normal'>Antoine Green</span></p>
 
                     </div>
                     <div className='grid md:grid grid-cols-2 md:gap-2 gap-1 '>
-                      <div className='bg-gray-200 md:mt-1 text-center p-2'>
+                      <div className='bg-white md:mt-1 text-center p-2'>
                         <p className='font-semibold'>Date of Birth : <span className='font-normal'>22/09/1997</span></p>
                       </div>
-                      <div className='bg-gray-200 md:mt-1 flex justify-center items-center p-2'>
+                      <div className='bg-white md:mt-1 flex justify-center items-center p-2'>
                         <p className='font-semibold'>Gender : <span className='font-normal'>Male</span></p>
                       </div>
 
@@ -142,13 +142,13 @@ function HomePage() {
                 </div>
                 {/* second div */}
                 <div className='flex justify-between items-center md:gap-2 gap-1 mt-1'>
-                  <div className='bg-gray-200 md:mt-1 flex justify-center items-center p-2 md:ms-20 md:w-full w-1/2'>
+                  <div className='bg-white md:mt-1 flex justify-center items-center p-2 md:ms-20 md:w-full w-1/2'>
                     <p className='font-semibold'>Attendence Percentage : <span className='font-normal'>98</span></p>
                   </div>
-                  <div className='bg-gray-200 md:mt-1 flex justify-center items-center md:p-2 py-5  w-1/3'>
+                  <div className='bg-white md:mt-1 flex justify-center items-center md:p-2 py-5  w-1/3'>
                     <p className='font-semibold'>Class : <span className='font-normal'>10 B</span></p>
                   </div>
-                  <div className='bg-gray-200 md:mt-1 flex justify-center items-center md:p-2 py-5 md:me-20  w-1/3'>
+                  <div className='bg-white md:mt-1 flex justify-center items-center md:p-2 py-5 md:me-20  w-1/3'>
                     <p className='font-semibold'>GPA : <span className='font-normal'>9</span></p>
                   </div>
 
@@ -243,10 +243,10 @@ function HomePage() {
 
                 {/* fourth div */}
                 <div className='flex justify-between items-center md:gap-2 gap-1 md:mt-5 mt-2 md:mx-20 '>
-                  <div className='bg-gray-200 md:mt-1 flex justify-center items-center md:p-2 py-5 w-full'>
+                  <div className='bg-white md:mt-1 flex justify-center items-center md:p-2 py-5 w-full'>
                     <p className='font-semibold'>Remarks : <span className='font-normal'>EHS</span></p>
                   </div>
-                  <div className='bg-gray-200 md:mt-1 flex justify-center items-center md:p-2 py-5 w-full'>
+                  <div className='bg-white md:mt-1 flex justify-center items-center md:p-2 py-5 w-full'>
                     <p className='font-semibold'>Status : <span className='font-normal'>Passed</span></p>
                   </div>
 
@@ -254,8 +254,8 @@ function HomePage() {
 
                 {/* buttons */}
                 <div className='flex justify-end md:me-20 gap-5 mb-15 mt-5'>
-                  <button className='bg-yellow-300 font-semibold px-4 py-1 text-white rounded hover:bg-yellow-500 transition cursor-pointer'>Edit</button>
-                  <button className='bg-red-500 font-semibold px-4 py-1 text-white rounded hover:bg-red-700 transition cursor-pointer'>Delete</button>
+                  <button className='bg-yellow-300 font-semibold px-4 py-1 text-white rounded hover:bg-yellow-500 transition cursor-pointer shadow'>Edit</button>
+                  <button className='bg-red-500 font-semibold px-4 py-1 text-white rounded hover:bg-red-700 transition cursor-pointer shadow'>Delete</button>
                 </div>
 
 
